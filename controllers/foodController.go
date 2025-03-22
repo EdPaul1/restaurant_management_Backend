1805a44036cb826846b08705965e90d08ded3ba8
EdPaul1 <saidarajab101@gmail.com>
2025-03-22 18:32:22 +0300

    Feat: Add Controllers: CreateFood & GetMenus

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -2,21 +2,25 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"math"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"restaurant.com/m/database"
 	"restaurant.com/m/models"
 )
 
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
 var validate = validator.New()
 
-func GetFoods() gin.HandlerFunc {
+func GetFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
@@ -33,17 +37,55 @@ func GetFoods() gin.HandlerFunc {
 	}
 }
 
-func GetFood() gin.HandlerFunc {
+func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		// var foods models.Food
-		// err := foodCollection.Find(ctx)
+
 	}
 }
 
 func CreateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+		var food models.Food
 
+		if err := c.BindJSON(&food); err != nil {
+
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationError := validate.Struct(food)
+
+		if validationError != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationError.Error()})
+			return
+		}
+
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+		defer cancel()
+
+		if err != nil {
+			msg := fmt.Sprintf("menu was not found")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		food.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.ID = primitive.NewObjectID()
+		var num = toFixed(*food.Price, 2) // Rounds the price to 2 decimal places
+		food.Price = &num
+
+		result, insertError := foodCollection.InsertOne(ctx, food)
+
+		if insertError != nil {
+			msg := fmt.Sprintf("food not inserted")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, result)
 	}
 }
 
@@ -55,8 +97,7 @@ func UpdateFood() gin.HandlerFunc {
 
 // func round(num float64) int {
 
-// }
-
-// func toFixed(num float64, precision int) float64 {
-
-// }
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(int(num*output)) / output
+}
